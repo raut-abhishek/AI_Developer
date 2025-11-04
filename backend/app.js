44d0e2a813d2424c74137b5f3b88a2fa949eb879
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-11-04 18:23:18 +0530

    server setup

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,22 @@
-import dotenv from 'dotenv';
-dotenv.config();
+
 import express from 'express';
+import morgan from 'morgan';
+
+
+// initialize express
+const app = express();
+
+
+app.use(morgan(dev));
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.get('/', (req, res)=>{
+    res.json('Hello World');
+});
+
+export default app;

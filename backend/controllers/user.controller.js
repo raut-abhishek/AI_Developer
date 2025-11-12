4a660298168539762c7b76c864a78166d9de9546
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-11-12 18:53:33 +0530

    redis setup

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import userModel from '../models/user.model.js';
 import * as userService from '../services/user.service.js';
 import {validationResult} from 'express-validator';
+import redisClient from '../services/redis.service.js';
 
 
 export const createUserController = async (req, res)=>{
@@ -58,4 +59,18 @@ export const profileController = async (req, res)=>{
     res.status(200).json({
         user : req.user
     })
+}
+
+
+export const logoutController = async (req, res)=>{
+    try {
+        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+        redisClient.set(token, 'logout', 'EX', 60*60*24);
+
+        res.status(200).json({message: 'Logged out successfully'})
+        
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 }

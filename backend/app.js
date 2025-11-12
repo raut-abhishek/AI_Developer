47d08f086498541b06b1f337b74ac7d73d2b5718
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-11-12 18:55:23 +0530

    created /user route

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,28 @@
 
 import express from 'express';
 import morgan from 'morgan';
+import connect from './db/db.js';
+import userRoutes from './routes/user.routes.js'
+import cookieParser from 'cookie-parser';
+
+
+connect();
 
 
 // initialize express
 const app = express();
 
 
-app.use(morgan(dev));
+app.use(morgan('dev'));
 
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+
+app.use('/user',userRoutes)
 
 
 app.get('/', (req, res)=>{

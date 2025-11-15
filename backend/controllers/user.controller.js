1619865616cc4bf810e7d470e3af336a5ba75b71
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-11-15 20:58:01 +0530

    hide password from console

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -16,6 +16,7 @@ export const createUserController = async (req, res)=>{
 
         const token = await user.generateJWT();
 
+        delete user._doc.password;
 
         res.status(201).json({user, token});
     } catch (error) {
@@ -47,7 +48,7 @@ export const loginController = async (req, res)=>{
         }
 
         const token = await user.generateJWT();
-
+        delete user._doc.password;
         res.status(200).json({user, token});
 
     } catch (error) {

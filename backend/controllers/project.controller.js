a86a40f4f5b5a6474d211b59cb18d5f87a10174c
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-11-15 20:57:35 +0530

    project controller function

---
+++ backend/controllers/project.controller.js
@@ -0,0 +1,30 @@
+import projectModel from '../models/project.model.js';
+import * as projectService from '../services/project.service.js';
+import { validationResult } from 'express-validator';
+import userModel from '../models/user.model.js'
+
+export const createProject = async (req, res)=>{
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    try {
+        const email = req.user.email;
+        const {name} = req.body;
+        const loggedInUser = await userModel.findOne({email});
+        const userId = loggedInUser._id;
+    
+    
+        const newProject = await projectService.createProject({name, userId});
+    
+        res.status(201).json(newProject);
+        
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+
+
+}

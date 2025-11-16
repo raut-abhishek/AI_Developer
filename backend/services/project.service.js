66b927fb1787d355d040480bc48e5f50e64ff158
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-11-16 20:20:29 +0530

    service function to get all projects for user id

--- backend/services/project.service.js
+++ backend/services/project.service.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import projectModel from '../models/project.model.js';
 
 export const createProject = async ({name, userId})=> {
@@ -20,3 +21,16 @@ export const createProject = async ({name, userId})=> {
 
     return project;
 }
+
+export const getAllProjectByUserId = async ({userId})=>{
+
+    if(!userId){
+        throw new Error('UserId is required')
+    }
+
+    const allUserProjects = await projectModel.find({users: userId})
+
+    return allUserProjects
+}
+
+

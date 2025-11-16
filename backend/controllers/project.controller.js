1be02b0767a2194c618319bd42c0098a5f0dba4c
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-11-16 20:14:18 +0530

    controller function to add users to project

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -44,3 +44,29 @@ export const getAllProject = async (req, res)=>{
         res.status(400).json({error:error.message})
     }
 }
+
+export const addUserToProject = async (req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    try {
+        const {projectId, users} = req.body;
+
+        const loggedInUser = await userModel.findOne({
+            email: req.user.email
+        })
+
+        const project = await projectService.addUsersToProject({
+            projectId,
+            users,
+            userId:loggedInUser._id
+        })
+
+        return res.status(200).json({project})
+
+        
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}

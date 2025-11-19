d6f849cd56ea9455253cfea16143e9881029cd2e
Shubham Raut <iamshubhamraut@gmail.com>
2025-11-19 23:17:36 +0530

    comment out console.log statement

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -40,7 +40,7 @@ export const getAllProject = async (req, res)=>{
 
         
     } catch (error) {
-        console.log(error)
+        // console.log(error)
         res.status(400).json({error:error.message})
     }
 }

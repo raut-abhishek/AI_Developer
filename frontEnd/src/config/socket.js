30d8823fbbea0845e4b90af948d0b041fad7974a
Shubham Raut <iamshubhamraut@gmail.com>
2025-11-24 19:17:27 +0530

    exported socket instance

--- frontEnd/src/config/socket.js
+++ frontEnd/src/config/socket.js
@@ -1,11 +1,8 @@
 import socket from 'socket.io-client';
 
-
 let socketInstance = null;
 
-
 export const initializeSocket = (projectId) => {
-
     socketInstance = socket(import.meta.env.VITE_API_URL, {
         auth: {
             token: localStorage.getItem('token')
@@ -14,15 +11,15 @@ export const initializeSocket = (projectId) => {
             projectId
         }
     });
-
     return socketInstance;
+};
 
-}
+export const getSocket = () => socketInstance;
 
 export const receiveMessage = (eventName, cb) => {
     socketInstance.on(eventName, cb);
-}
+};
 
 export const sendMessage = (eventName, data) => {
     socketInstance.emit(eventName, data);
-}
+};

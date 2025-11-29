cbbd5841e2df1864e5f49d74224a7c10b711b063
raut-abhishek <iamabhishekraut01@gmail.com>
2025-11-29 14:11:29 +0530

    added headers fro webcontainer

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -4,5 +4,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),],
+  server:{
+    headers:{
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin"
+    }
+  }
 })

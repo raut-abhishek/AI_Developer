fb4c7146a2a7b563f836a85c08e237cc547e3f98
raut-abhishek <iamabhishekraut01@gmail.com>
2025-11-30 10:02:46 +0530

    aerver modified

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import mongoose from 'mongoose';
 import projectModel from './models/project.model.js';
 import { generateResult } from './services/ai.service.js';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
 const server = http.createServer(app);
@@ -98,6 +98,6 @@ io.on('connection', socket => {
 });
 
 
-server.listen(port, ()=>{
+server.listen(port, "0.0.0.0", ()=>{
     console.log(`Server is running on ${port}`);
 });

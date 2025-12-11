bb65e0e6abce726cc65d786a16513959bdfed0ee
raut-abhishek <iamabhishekraut01@gmail.com>
2025-12-11 18:15:40 +0530

    axios backend url issue fixed

--- frontEnd/src/config/axios.js
+++ frontEnd/src/config/axios.js
@@ -1,10 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
+
+const token = localStorage.getItem("token");
 
 const axiosInstance = axios.create({
-    baseURL : import.meta.env.VITE_API_URL,
-    headers: {
-        "Authorization": `Bearer ${localStorage.getItem('token')}`
-    }
-})
+  baseURL: import.meta.env.VITE_API_URL || window.location.origin,
+  headers: {
+    Authorization: token ? `Bearer ${token}` : "",
+  },
+  withCredentials: true,
+});
 
 export default axiosInstance;

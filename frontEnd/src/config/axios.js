2cae0c00704b0e31bbe024ebe245db092afdbc60
raut-abhishek <iamabhishekraut01@gmail.com>
2025-12-24 19:30:11 +0530

    modified axios instance

--- frontEnd/src/config/axios.js
+++ frontEnd/src/config/axios.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
-const token = localStorage.getItem("token");
-
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || window.location.origin,
-  headers: {
-    Authorization: token ? `Bearer ${token}` : "",
-  },
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axiosInstance;
